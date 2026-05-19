0087008546078821ef7722be878163c823574f92
vahid2104 <vahid.aliyev.03@mail.ru>
2026-05-19 09:06:30 +0200

    chore: initialize Next.js weather dashboard project

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+  return (
+    <main className="flex justify-center items-center min-h-screen bg-[#2b2b2b] px-10 py10">
+      <div className="mx-auto h-190 w-305 rounded-[10px] bg-[#0f0f15] p-5 shadow-2xl">Dashboard UI</div>
+    </main>
+  );
+};
+
+export default Dashboard;
